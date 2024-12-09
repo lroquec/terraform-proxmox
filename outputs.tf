9a34b9a49daa2c44bc4a84667ba60a3d219856a8
Laura Roque <lroquecu@gmail.com>
2024-12-09 18:24:54 +0100

    correct output

--- outputs.tf
+++ outputs.tf
@@ -1,6 +1,7 @@
 output "vm_ips" {
   value = [
-    for vm in proxmox_virtual_environment_vm.k8s-servers : vm.initialization[0].ip_config[0].ipv4.address
+    for vm in proxmox_virtual_environment_vm.k8s-servers :
+    [for ipv4 in vm.initialization[0].ip_config[0].ipv4 : ipv4.address]
   ]
   description = "List of IP addresses of the created VMs"
 }
