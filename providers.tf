54f5623d925428fe9b39dd2eb17c136d0c5e511f
Laura Roque <lroquecu@gmail.com>
2024-12-09 16:58:27 +0100

    add providers file

---
+++ providers.tf
@@ -0,0 +1,19 @@
+terraform {
+  required_providers {
+    proxmox = {
+      source  = "bpg/proxmox"
+      version = "0.68.0"
+    }
+  }
+}
+provider "proxmox" {
+  endpoint = "https://pv02:8006"
+  insecure = true
+  username = "root@pam"
+  password = var.root_password
+  ssh {
+    agent       = false
+    private_key = file("~/.ssh/id_ed25519")
+    username    = "root"
+  }
+}
