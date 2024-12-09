3d19d177939041855af99539449a0025557fad28
Laura Roque <lroquecu@gmail.com>
2024-12-09 16:59:17 +0100

    variables

---
+++ variables.tf
@@ -0,0 +1,16 @@
+variable "root_password" {
+  description = "The root password for the VM"
+  type        = string
+}
+variable "ssh_key" {
+  description = "The SSH key for the VM"
+  type        = string
+  default     = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIE/Pjg7YXZ8Yau9heCc4YWxFlzhThnI+IhUx2hLJRxYE Cloud-Init@Terraform"
+}
+
+variable "proxmox_host" {
+  default = "pve2"
+}
+variable "template_id" {
+  default = 9000
+}
